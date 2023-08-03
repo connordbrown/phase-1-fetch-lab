17e32bf15c222533f0daf8008600d21495026d03
Connor <connordbrown@gmail.com>
2023-08-03 16:58:01 -0600

    completes lab

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+   const bookRun = fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(data => {
+      renderBooks(data);
+
+      console.log(data);
+      pageCount(data);
+      pageCounter(data);
+      getBook(data, 1);
+      getCharacter(data, 1031);
+    });
+  return bookRun;
 }
 
 function renderBooks(books) {
@@ -15,3 +25,37 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+// my reducer function - gets total number of pages in series
+function pageCount(books) {
+  let pageCount = 0;
+  for (const book of books) {
+    pageCount += book.numberOfPages;
+  }
+  console.log(`Total number of pages: ${pageCount}`);
+}
+
+// native reducer function - gets total number of pages in series
+function pageCounter(books) {
+  const pageCount = books.reduce((pageCounter, book) => pageCounter + book.numberOfPages
+  , 0);
+  console.log(`Total number of pages: ${pageCount}`);
+}
+
+// retrieves desired book in series
+function getBook(books, bookNum) {
+  console.log(books[bookNum - 1])
+}
+
+// gets desired chracter in the series
+function getCharacter(books, charNum) {
+  let charCtr = 0;
+  for (let book of books) {
+    for (let char of book.characters) {
+      charCtr += 1;
+      if (charCtr === charNum) {
+        console.log(`THIS ONE: ${char}, #${charCtr}`);
+      }
+    }
+  }
+}
